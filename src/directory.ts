d2d3510fc5095bd64b36cafed4c2973dac5a5e3a
willgd <56427283+willgd-git@users.noreply.github.com>
2025-05-04 01:53:13 +0700

    Update directory.ts

--- src/directory.ts
+++ src/directory.ts
@@ -370,4 +370,14 @@ export const projects: Project[] = [
     x: 'https://x.com/latticexyz',
     tags: [],
   },
+  {
+    type: 'project',
+    name: 'Centuari',
+    blurb: 'Centuari is a Central Limit Order Book (CLOB) powered lending platform that aims to bring the best of both Traditional Finance (TradFi) and decentralization technology. Our platform powers fixed-rate loans – delivering stable, predictable yields for both lenders and borrowers – and provide tailored and advanced financial solutions through our curator-managed vaults and uncollateralized lending products.',
+    logo: '/projects/centuari/logo.png',
+    banner: '/projects/centuari/banner.png',
+    status: 'coming soon',
+    x: 'https://x.com/CentuariLabs',
+    tags: ["DeFi", "DAO", "Yield"],
+  }
 ];
