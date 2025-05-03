63795c6c3b78757a1ba586ced1c69e247fcde9d0
willgd <56427283+willgd-git@users.noreply.github.com>
2025-05-04 01:57:18 +0700

    Update directory.ts

--- src/directory.ts
+++ src/directory.ts
@@ -373,7 +373,7 @@ export const projects: Project[] = [
   {
     type: 'project',
     name: 'Centuari',
-    blurb: 'Centuari is a Central Limit Order Book (CLOB) powered lending platform that aims to bring the best of both Traditional Finance (TradFi) and decentralization technology. Our platform powers fixed-rate loans – delivering stable, predictable yields for both lenders and borrowers – and provide tailored and advanced financial solutions through our curator-managed vaults and uncollateralized lending products.',
+    blurb: 'Fixed-Rate Order Book Lending with Curated Vaults and Trust-Based Uncollateralized Borrowing',
     logo: '/projects/centuari/logo.png',
     banner: '/projects/centuari/banner.png',
     status: 'coming soon',
